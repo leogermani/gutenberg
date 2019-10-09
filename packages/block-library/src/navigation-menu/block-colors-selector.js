0c65116febcd5db9ba5d833e50b31be5ecfcc93e
retrofox <rdsuarez@gmail.com>
2019-10-09 13:34:37 -0300

    navigation-menu: improve colors-selector component

---
+++ packages/block-library/src/navigation-menu/block-colors-selector.js
@@ -0,0 +1,78 @@
+/**
+ * WordPress dependencies
+ */
+import { IconButton, Dropdown, Toolbar } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { DOWN } from '@wordpress/keycodes';
+import { ColorPaletteControl } from '@wordpress/block-editor';
+
+/**
+ * Color Selector Icon component.
+ * @return {*} React Icon component.
+ * @constructor
+ */
+const ColorSelectorIcon = () =>
+	<div className="block-editor-block-colors-selector__icon-container">
+		<div className="colors-selector__state-selection">
+			{ __( 'Aa' ) }
+		</div>
+	</div>;
+
+/**
+ * Renders the Colors Selector Toolbar with the icon button.
+ *
+ * @param {function} onToggle Callback open/close Dropdown.
+ * @param {bool} isOpen True is the color settings dropdown is open. Otherwise, False.
+ * @return {*} React toggle button component.
+ */
+const renderToggle = ( { onToggle, isOpen } ) => {
+	const openOnArrowDown = ( event ) => {
+		if ( ! isOpen && event.keyCode === DOWN ) {
+			event.preventDefault();
+			event.stopPropagation();
+			onToggle();
+		}
+	};
+
+	return (
+		<Toolbar>
+			<IconButton
+				className="components-icon-button components-toolbar__control block-editor-block-colors-selector__toggle"
+				label={ __( 'Open Colors Selector' ) }
+				onClick={ onToggle }
+				onKeyDown={ openOnArrowDown }
+				icon={ <ColorSelectorIcon/> }
+			/>
+		</Toolbar>
+	);
+};
+
+const renderContent = () => (
+	<>
+		<div className="color-palette-controller-container">
+			<ColorPaletteControl
+				value="#aaa"
+				onChange= { console.log }
+				label={ __( 'Background Color' ) }
+			/>
+		</div>
+
+		<div className="color-palette-controller-container">
+			<ColorPaletteControl
+				value="#aaa"
+				onChange= { console.log }
+				label={ __( 'Text Color' ) }
+			/>
+		</div>
+	</>
+);
+
+export default () =>
+	<Dropdown
+		position="bottom right"
+		className="editor-block-colors-selector block-editor-block-colors-selector"
+		contentClassName="editor-block-colors-selector__popover block-editor-block-colors-selector__popover"
+		renderToggle={ renderToggle }
+		renderContent={ renderContent }
+	/>;
+
