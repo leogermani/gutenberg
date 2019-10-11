6b45cf2e55d81c80b27c2f3b6b3d2f39b972d42a
lukewalczak <lukasz.walczak.pwr@gmail.com>
2019-10-11 10:04:03 +0200

    Add logic to control spacer height

--- packages/block-library/src/spacer/edit.native.js
+++ packages/block-library/src/spacer/edit.native.js
@@ -9,8 +9,9 @@ import { View } from 'react-native';
  */
 import {
 	PanelBody,
+	BottomSheet,
 } from '@wordpress/components';
-
+import { useState } from '@wordpress/element';
 import {
 	InspectorControls,
 } from '@wordpress/block-editor';
@@ -21,21 +22,41 @@ import { __ } from '@wordpress/i18n';
  */
 import styles from './editor.scss';
 
-const getInspectorControls = () => (
-	<InspectorControls>
-		<PanelBody title={ __( 'SpacerSettings' ) } >
-		</PanelBody>
-	</InspectorControls>
-);
+const minSpacerHeight = 20;
+const maxSpacerHeight = 500;
 
-export class SpacerEdit extends React.Component {
-	render() {
-		return (
-			<View style={ [ styles.staticSpacer, { flex: 1 } ] }>
-				{ getInspectorControls() }
-			</View>
-		);
-	}
-}
+const SpacerEdit = ( { isSelected } ) => {
+	const [ spacerHeight, setSpacerHeight ] = useState( 20 );
+
+	const changeSpacerHeight = ( value ) => {
+		if ( value < minSpacerHeight ) {
+			return setSpacerHeight( minSpacerHeight );
+		} else if ( value > maxSpacerHeight ) {
+			return setSpacerHeight( maxSpacerHeight );
+		} return setSpacerHeight( value );
+	};
+
+	const getInspectorControls = () => (
+		<InspectorControls>
+			<PanelBody title={ __( 'SpacerSettings' ) } >
+				<BottomSheet.RangeCell
+					icon={ 'admin-settings' }
+					label={ __( 'Slider' ) }
+					value={ spacerHeight }
+					minimumValue={ minSpacerHeight }
+					maximumValue={ maxSpacerHeight }
+					separatorType={ 'fullWidth' }
+					onChangeValue={ ( value ) => changeSpacerHeight( value ) }
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+
+	return (
+		<View style={ [ styles.staticSpacer, isSelected && styles.selectedSpacer, { height: spacerHeight } ] }>
+			{ getInspectorControls() }
+		</View>
+	);
+};
 
 export default SpacerEdit;
